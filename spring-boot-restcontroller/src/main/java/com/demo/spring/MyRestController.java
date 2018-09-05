e93cfa1479d8446034ed90ba4421e0a3a5253b6a
Daya <catchmycity@gmail.com>
2018-09-05 17:07:27 +0530

    Spring boot Microservice @RestController

--- spring-boot-restcontroller/src/main/java/com/demo/spring/MyRestController.java
+++ spring-boot-restcontroller/src/main/java/com/demo/spring/MyRestController.java
@@ -4,6 +4,8 @@ import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -31,6 +33,11 @@ public class MyRestController {
 		Employee emp = new Employee();
 		emp.setCity("chennai");
 		emp.setName("John");
+		return emp;
+	}
+
+	@PostMapping(path = "/saveEmployee", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_XML_VALUE)
+	public @ResponseBody Employee getEmployeeInXML(@RequestBody Employee emp) {
 		return emp;
 	}
 }
